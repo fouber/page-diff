c8a113f23990fde8d8ce28066c87ed301664ccf4
fouber <fouber.net@gmail.com>
2020-09-17 21:17:51 +0800

    修复重大bug

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
 exports.diff = require('./lib/diff');
 exports.walk = require('./lib/walk');
-exports.highlight = require('./lib/highlight');
-
-if(typeof phantom !== 'undefined'){
-    var root = phantom.libraryPath;
-} else {
-    root = __dirname;
-}
-exports.LIBRARY_PATH = root + '/lib';
-exports.DIFF_SCRIPT_PATH = root + '/lib/diff.js';
-exports.WALK_SCRIPT_PATH = root + '/lib/walk.js';
-exports.HIGHLIGHT_SCRIPT_PATH = root + '/lib/highlight.js';
